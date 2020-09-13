9c6d2b55bd4f19e3bf1589033e6efc23bca96c10
z1045553015 <z1045553015@gmail.com>
2020-09-13 00:04:43 -0500

    ms3

--- Data/Menu.cs
+++ Data/Menu.cs
@@ -10,8 +10,15 @@ using System.Linq;
 
 namespace BleakwindBuffet.Data
 {
+    /// <summary>
+    ///  A class represent the menu 
+    /// </summary>
    public static class Menu
     {
+        /// <summary>
+        /// A method to return all entrees
+        /// </summary>
+        /// <returns>an IEnumerable<IOrderItem> containing an instance of all available entrees offered by Bleakwind Buffet</returns>
 
         public static IEnumerable<IOrderItem> Entrees()
         {
@@ -47,10 +54,14 @@ namespace BleakwindBuffet.Data
 
 
         }
-
-       public static IEnumerable<IOrderItem> Drinks()
+        /// <summary>
+        /// A method return all drinks
+        /// </summary>
+        /// <returns>an IEnumerable<IOrderItem> containing all available drinks offered by BleakwindBuffet. As each drink has 3 different sizes, this collection should contain a small, medium, and large instance of each. Similarly, it should contain three of each flavor of SailorSoda: one large, one medium, and one small.</returns>
+        public static IEnumerable<IOrderItem> Drinks()
         {
             IEnumerable<IOrderItem> drinks = new List<IOrderItem>();
+
             AretinoAppleJuice saa = new AretinoAppleJuice();
             AretinoAppleJuice maa = new AretinoAppleJuice();
             AretinoAppleJuice laa = new AretinoAppleJuice();
@@ -63,12 +74,173 @@ namespace BleakwindBuffet.Data
             drinks.ToList().Add(SAA);
             drinks.ToList().Add(MAA);
             drinks.ToList().Add(LAA);
+
+            WarriorWater sww = new WarriorWater();
+            WarriorWater mww = new WarriorWater();
+            WarriorWater lww = new WarriorWater();
+            sww.Size = Size.Small;
+            mww.Size = Size.Medium;
+            lww.Size = Size.Large;
+            IOrderItem SWW = (IOrderItem)sww;
+            IOrderItem MWW = (IOrderItem)mww;
+            IOrderItem LWW = (IOrderItem)lww;
+            drinks.ToList().Add(SWW);
+            drinks.ToList().Add(MWW);
+            drinks.ToList().Add(LWW);
+
+            MarkarthMilk smm = new MarkarthMilk();
+            MarkarthMilk mmm = new MarkarthMilk();
+            MarkarthMilk lmm = new MarkarthMilk();
+            smm.Size = Size.Small;
+            mmm.Size = Size.Medium;
+            lmm.Size = Size.Large;
+            IOrderItem SMM = (IOrderItem)smm;
+            IOrderItem MMM = (IOrderItem)mmm;
+            IOrderItem LMM = (IOrderItem)lmm;
+            drinks.ToList().Add(SMM);
+            drinks.ToList().Add(MMM);
+            drinks.ToList().Add(LMM);
+
+            CandlehearthCoffee scc = new CandlehearthCoffee();
+            CandlehearthCoffee mcc = new CandlehearthCoffee();
+            CandlehearthCoffee lcc = new CandlehearthCoffee();
+            scc.Size = Size.Small;
+            mcc.Size = Size.Medium;
+            lcc.Size = Size.Large;
+            IOrderItem SCC = (IOrderItem)scc;
+            IOrderItem MCC = (IOrderItem)mcc;
+            IOrderItem LCC = (IOrderItem)lcc;
+            drinks.ToList().Add(SCC);
+            drinks.ToList().Add(MCC);
+            drinks.ToList().Add(LCC);
+
+            CandlehearthCoffee sdc = new CandlehearthCoffee();
+            CandlehearthCoffee mdc = new CandlehearthCoffee();
+            CandlehearthCoffee ldc = new CandlehearthCoffee();
+            sdc.Decaf = true;
+            mdc.Decaf = true;
+            ldc.Decaf = true;
+            sdc.Size = Size.Small;
+            mdc.Size = Size.Medium;
+            ldc.Size = Size.Large;
+            IOrderItem SDC = (IOrderItem)sdc;
+            IOrderItem MDC = (IOrderItem)mdc;
+            IOrderItem LDC = (IOrderItem)ldc;
+            drinks.ToList().Add(SDC);
+            drinks.ToList().Add(MDC);
+            drinks.ToList().Add(LDC);
+
+
+            SailorSoda scherry = new SailorSoda();
+            SailorSoda mcherry = new SailorSoda();
+            SailorSoda lcherry = new SailorSoda();
+            scherry.Size = Size.Small;
+            mcherry.Size = Size.Medium;
+            lcherry.Size = Size.Large;
+            IOrderItem SCherry = (IOrderItem)scherry;
+            IOrderItem MCherry = (IOrderItem)mcherry;
+            IOrderItem LCherry = (IOrderItem)lcherry;
+            drinks.ToList().Add(SCherry);
+            drinks.ToList().Add(MCherry);
+            drinks.ToList().Add(LCherry);
+
+
+            SailorSoda sblac = new SailorSoda();
+            SailorSoda mblac = new SailorSoda();
+            SailorSoda lblac = new SailorSoda();
+            sblac.Size = Size.Small;
+            mblac.Size = Size.Medium;
+            lblac.Size = Size.Large;
+            sblac.Flavor = SodaFlavor.Blackberry;
+            mblac.Flavor = SodaFlavor.Blackberry;
+            lblac.Flavor = SodaFlavor.Blackberry;
+            IOrderItem SBlac = (IOrderItem)sblac;
+            IOrderItem MBlac = (IOrderItem)mblac;
+            IOrderItem LBlac = (IOrderItem)lblac;
+            drinks.ToList().Add(SBlac);
+            drinks.ToList().Add(MBlac);
+            drinks.ToList().Add(LBlac);
+
+
+
+            SailorSoda sgra = new SailorSoda();
+            SailorSoda mgra = new SailorSoda();
+            SailorSoda lgra = new SailorSoda();
+            sgra.Flavor = SodaFlavor.Grapefruit;
+            mgra.Flavor = SodaFlavor.Grapefruit;
+            lgra.Flavor = SodaFlavor.Grapefruit;
+            sgra.Size = Size.Small;
+            mgra.Size = Size.Medium;
+            lgra.Size = Size.Large;
+            IOrderItem SGra = (IOrderItem)sgra;
+            IOrderItem MGra = (IOrderItem)mgra;
+            IOrderItem LGra = (IOrderItem)lgra;
+            drinks.ToList().Add(SGra);
+            drinks.ToList().Add(MGra);
+            drinks.ToList().Add(LGra);
+
+            SailorSoda slem = new SailorSoda();
+            SailorSoda mlem = new SailorSoda();
+            SailorSoda llem = new SailorSoda();
+            slem.Flavor = SodaFlavor.Lemon;
+            mlem.Flavor = SodaFlavor.Lemon;
+            llem.Flavor = SodaFlavor.Lemon;
+            slem.Size = Size.Small;
+            mlem.Size = Size.Medium;
+            llem.Size = Size.Large;
+            IOrderItem SLem = (IOrderItem)slem;
+            IOrderItem MLem = (IOrderItem)mlem;
+            IOrderItem LLem = (IOrderItem)llem;
+            drinks.ToList().Add(SLem);
+            drinks.ToList().Add(MLem);
+            drinks.ToList().Add(LLem);
+
+            SailorSoda spea = new SailorSoda();
+            SailorSoda mpea = new SailorSoda();
+            SailorSoda lpea = new SailorSoda();
+            spea.Size = Size.Small;
+            mpea.Size = Size.Medium;
+            lpea.Size = Size.Large;
+            spea.Flavor = SodaFlavor.Peach;
+            mpea.Flavor = SodaFlavor.Peach;
+            lpea.Flavor = SodaFlavor.Peach;
+            IOrderItem SPea = (IOrderItem)spea;
+            IOrderItem MPea = (IOrderItem)mpea;
+            IOrderItem LPea = (IOrderItem)lpea;
+            drinks.ToList().Add(SPea);
+            drinks.ToList().Add(MPea);
+            drinks.ToList().Add(LPea);
+
+
+
+            SailorSoda swat = new SailorSoda();
+            SailorSoda mwat = new SailorSoda();
+            SailorSoda lwat = new SailorSoda();
+            swat.Flavor = SodaFlavor.Watermelon;
+            mwat.Flavor = SodaFlavor.Watermelon;
+            lwat.Flavor = SodaFlavor.Watermelon;
+            swat.Size = Size.Small;
+            mwat.Size = Size.Medium;
+            lwat.Size = Size.Large;
+            IOrderItem SWat = (IOrderItem)swat;
+            IOrderItem MWat = (IOrderItem)mwat;
+            IOrderItem LWat = (IOrderItem)lwat;
+            drinks.ToList().Add(SWat);
+            drinks.ToList().Add(MWat);
+            drinks.ToList().Add(LWat);
+
+
+
             return drinks;
 
 
 
 
         }
+        /// <summary>
+        /// A method return all sides
+        /// </summary>
+        /// <returns>an IEnumerable<IOrderItem> containing an instance of all available sides offered by Bleakwind Buffet. As each side has 3 different sizes, this collection should contain a small, medium, and large instance of each.</returns>
         public static IEnumerable<IOrderItem> Sides()
         {
             IEnumerable<IOrderItem> sides = new List<IOrderItem>();
@@ -145,6 +317,17 @@ namespace BleakwindBuffet.Data
 
             return sides;
 
+
+        }
+        public static IEnumerable<IOrderItem> FullMenu()
+        {
+            
+            IEnumerable<IOrderItem> drinks = Drinks();
+            IEnumerable<IOrderItem> sides = Sides();
+            IEnumerable<IOrderItem> entrees = Entrees();
+            IEnumerable<IOrderItem> drinksAndsides = drinks.Concat(sides);
+            IEnumerable<IOrderItem> full = entrees.Concat(drinksAndsides);
+            return full;
 
         }
     }
--- DataTests/UnitTests/MenuTests.cs
+++ DataTests/UnitTests/MenuTests.cs
@@ -9,6 +9,9 @@ using Xunit;
 
 namespace BleakwindBuffet.DataTests.UnitTests
 {
+    /// <summary>
+    /// unit text for menu.cs
+    /// </summary>
     public class MenuTests
     {
         [Fact]
@@ -45,6 +48,124 @@ namespace BleakwindBuffet.DataTests.UnitTests
             Assert.All(result, item => item.ToString().Contains("Large Mad Otar Grits"));
 
 
-        } 
+        }
+        [Fact]
+        public void ShouldReturnAllDrinks()
+        {
+            IEnumerable<IOrderItem> result = Menu.Drinks();
+            Assert.All(result, item => item.ToString().Contains("Small Aretino Apple Juice"));
+            Assert.All(result, item => item.ToString().Contains("Medium Aretino Apple Juice"));
+            Assert.All(result, item => item.ToString().Contains("Large Aretino Apple Juice"));
+
+            Assert.All(result, item => item.ToString().Contains("Small Candlehearth Coffee"));
+            Assert.All(result, item => item.ToString().Contains("Medium Candlehearth Coffee"));
+            Assert.All(result, item => item.ToString().Contains("Large Candlehearth Coffee"));
+            Assert.All(result, item => item.ToString().Contains("Small Decaf Candlehearth Coffee"));
+            Assert.All(result, item => item.ToString().Contains("Medium Decaf Candlehearth Coffee"));
+            Assert.All(result, item => item.ToString().Contains("Large Decaf Candlehearth Coffee"));
+
+            Assert.All(result, item => item.ToString().Contains("Small Markarth Milk"));
+            Assert.All(result, item => item.ToString().Contains("Medium Markarth Milk"));
+            Assert.All(result, item => item.ToString().Contains("Large Markarth Milk"));
+
+            Assert.All(result, item => item.ToString().Contains("Small Warrior Water"));
+            Assert.All(result, item => item.ToString().Contains("Medium Warrior Water"));
+            Assert.All(result, item => item.ToString().Contains("Large Warrior Water"));
+
+            Assert.All(result, item => item.ToString().Contains("Small Cherry Sailor Soda"));
+            Assert.All(result, item => item.ToString().Contains("Medium Cherry Sailor Soda"));
+            Assert.All(result, item => item.ToString().Contains("Large Cherry Sailor Soda"));
+
+            Assert.All(result, item => item.ToString().Contains("Small Blackberry Sailor Soda"));
+            Assert.All(result, item => item.ToString().Contains("Medium Blackberry Sailor Soda"));
+            Assert.All(result, item => item.ToString().Contains("Large Blackberry Sailor Soda"));
+
+            Assert.All(result, item => item.ToString().Contains("Small Grapefruit Sailor Soda"));
+            Assert.All(result, item => item.ToString().Contains("Medium Grapefruit Sailor Soda"));
+            Assert.All(result, item => item.ToString().Contains("Large Grapefruit Sailor Soda"));
+
+            Assert.All(result, item => item.ToString().Contains("Small Lemon Sailor Soda"));
+            Assert.All(result, item => item.ToString().Contains("Medium Lemon Sailor Soda"));
+            Assert.All(result, item => item.ToString().Contains("Large Lemon Sailor Soda"));
+
+            Assert.All(result, item => item.ToString().Contains("Small Peach Sailor Soda"));
+            Assert.All(result, item => item.ToString().Contains("Medium Peach Sailor Soda"));
+            Assert.All(result, item => item.ToString().Contains("Large Peach Sailor Soda"));
+
+            Assert.All(result, item => item.ToString().Contains("Small Watermelon Sailor Soda"));
+            Assert.All(result, item => item.ToString().Contains("Medium Watermelon Sailor Soda"));
+            Assert.All(result, item => item.ToString().Contains("Large Watermelon Sailor Soda"));
+
+        }
+        [Fact]
+        public void ShouldReturnAllItems()
+        {
+            IEnumerable<IOrderItem> result = Menu.FullMenu();
+            Assert.All(result, item => item.ToString().Contains("Briarheart Burger"));
+            Assert.All(result, item => item.ToString().Contains("Double Draugr"));
+            Assert.All(result, item => item.ToString().Contains("Garden Orc Omelette"));
+            Assert.All(result, item => item.ToString().Contains("Philly Poacher"));
+            Assert.All(result, item => item.ToString().Contains("Smokehouse Skeleton"));
+            Assert.All(result, item => item.ToString().Contains("Thalmor Triple"));
+            Assert.All(result, item => item.ToString().Contains("Thugs T-Bone"));
+
+            Assert.All(result, item => item.ToString().Contains("Small Vokun Salad"));
+            Assert.All(result, item => item.ToString().Contains("Medium Vokun Salad"));
+            Assert.All(result, item => item.ToString().Contains("Large Vokun Salad"));
+            Assert.All(result, item => item.ToString().Contains("Small Dragonborn Waffle Fries"));
+            Assert.All(result, item => item.ToString().Contains("Medium Dragonborn Waffle Fries"));
+            Assert.All(result, item => item.ToString().Contains("Large Dragonborn Waffle Fries"));
+            Assert.All(result, item => item.ToString().Contains("Small Fried Miraak"));
+            Assert.All(result, item => item.ToString().Contains("Medium Fried Miraak"));
+            Assert.All(result, item => item.ToString().Contains("Large Fried Miraak"));
+            Assert.All(result, item => item.ToString().Contains("Small Mad Otar Grits"));
+            Assert.All(result, item => item.ToString().Contains("Medium Mad Otar Grits"));
+            Assert.All(result, item => item.ToString().Contains("Large Mad Otar Grits"));
+
+            Assert.All(result, item => item.ToString().Contains("Small Aretino Apple Juice"));
+            Assert.All(result, item => item.ToString().Contains("Medium Aretino Apple Juice"));
+            Assert.All(result, item => item.ToString().Contains("Large Aretino Apple Juice"));
+
+            Assert.All(result, item => item.ToString().Contains("Small Candlehearth Coffee"));
+            Assert.All(result, item => item.ToString().Contains("Medium Candlehearth Coffee"));
+            Assert.All(result, item => item.ToString().Contains("Large Candlehearth Coffee"));
+            Assert.All(result, item => item.ToString().Contains("Small Decaf Candlehearth Coffee"));
+            Assert.All(result, item => item.ToString().Contains("Medium Decaf Candlehearth Coffee"));
+            Assert.All(result, item => item.ToString().Contains("Large Decaf Candlehearth Coffee"));
+
+            Assert.All(result, item => item.ToString().Contains("Small Markarth Milk"));
+            Assert.All(result, item => item.ToString().Contains("Medium Markarth Milk"));
+            Assert.All(result, item => item.ToString().Contains("Large Markarth Milk"));
+
+            Assert.All(result, item => item.ToString().Contains("Small Warrior Water"));
+            Assert.All(result, item => item.ToString().Contains("Medium Warrior Water"));
+            Assert.All(result, item => item.ToString().Contains("Large Warrior Water"));
+
+            Assert.All(result, item => item.ToString().Contains("Small Cherry Sailor Soda"));
+            Assert.All(result, item => item.ToString().Contains("Medium Cherry Sailor Soda"));
+            Assert.All(result, item => item.ToString().Contains("Large Cherry Sailor Soda"));
+
+            Assert.All(result, item => item.ToString().Contains("Small Blackberry Sailor Soda"));
+            Assert.All(result, item => item.ToString().Contains("Medium Blackberry Sailor Soda"));
+            Assert.All(result, item => item.ToString().Contains("Large Blackberry Sailor Soda"));
+
+            Assert.All(result, item => item.ToString().Contains("Small Grapefruit Sailor Soda"));
+            Assert.All(result, item => item.ToString().Contains("Medium Grapefruit Sailor Soda"));
+            Assert.All(result, item => item.ToString().Contains("Large Grapefruit Sailor Soda"));
+
+            Assert.All(result, item => item.ToString().Contains("Small Lemon Sailor Soda"));
+            Assert.All(result, item => item.ToString().Contains("Medium Lemon Sailor Soda"));
+            Assert.All(result, item => item.ToString().Contains("Large Lemon Sailor Soda"));
+
+            Assert.All(result, item => item.ToString().Contains("Small Peach Sailor Soda"));
+            Assert.All(result, item => item.ToString().Contains("Medium Peach Sailor Soda"));
+            Assert.All(result, item => item.ToString().Contains("Large Peach Sailor Soda"));
+
+            Assert.All(result, item => item.ToString().Contains("Small Watermelon Sailor Soda"));
+            Assert.All(result, item => item.ToString().Contains("Medium Watermelon Sailor Soda"));
+            Assert.All(result, item => item.ToString().Contains("Large Watermelon Sailor Soda"));
+
+        }
+    
     }
 }
