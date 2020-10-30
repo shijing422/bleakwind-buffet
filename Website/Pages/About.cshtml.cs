2f0236431289fc0a471c7ab4fd52e4e905bd384e
z1045553015 <z1045553015@gmail.com>
2020-10-29 22:41:43 -0500

    w1

---
+++ Website/Pages/About.cshtml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace Website.Pages
+{
+    public class AboutModel : PageModel
+    {
+        private readonly ILogger<PrivacyModel> _logger;
+
+        public AboutModel(ILogger<PrivacyModel> logger)
+        {
+            _logger = logger;
+        }
+        public void OnGet()
+        {
+
+        }
+    }
+}
